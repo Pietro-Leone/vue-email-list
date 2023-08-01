54f10b90d3b539c0b30ef12b06d4434a1ad9ab44
Pietro-Leone <leonepietro1999@gmail.com>
2023-08-01 17:42:06 +0200

    fine esercizio

--- js/main.js
+++ js/main.js
@@ -4,13 +4,22 @@ Vue.createApp({
 
   data() {
     return {
-
+      mailList: [],
     };
   },
   methods: {
 
   },
   mounted() {
-
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get(
+          "https://flynn.boolean.careers/exercises/api/random/mail"
+        )
+        .then((mail) => {
+          this.mailList.push(mail.data.response);
+        }
+        )
+    }
   },
 }).mount("#app");
